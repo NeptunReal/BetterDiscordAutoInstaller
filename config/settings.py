f44ffeca5bc27a8c035059dd609b0e7b2195c230
NeptunReal <sabbneptun@gmail.com>
2025-05-23 01:58:32 -0600

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ WORKFLOW_RUNS_LIMIT: int = 5
 RERUN_DISCORD_EDITION: Optional[str] = config.DiscordEdition.STABLE.to_str()
 
 DISCORD_PARENT_PATH: Optional[str] = None
-DISCORD_CANARY_PARENT_PATH: Optional[str] = None
+DISCORD_CANARY_PARENT_PATH: Optional[str] = C:\Users\Hunti\AppData\Local\DiscordCanary\app-1.0.512
 DISCORD_PTB_PARENT_PATH: Optional[str] = None
 LAST_INSTALLED_DISCORD_VERSION: Optional[str] = None
 LAST_INSTALLED_DISCORD_CANARY_VERSION: Optional[str] = None
